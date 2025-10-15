7df823a5b29751072aeec8dae3a71d99f1e98d4e
MrFrangipane <contact@frangipane.me>
2025-10-16 01:11:30 +0200

    shared memory and multiprocessing

--- ledboardtranslatoremulator/midi/process.py
+++ ledboardtranslatoremulator/midi/process.py
@@ -1,23 +1,25 @@
-from multiprocessing import shared_memory
+from multiprocessing.shared_memory import SharedMemory
 
 import mido
 
 
 def process_midi(shared_buffer_name):
-    shared_memory_buffer = shared_memory.SharedMemory(name=shared_buffer_name)
+    shared_memory = SharedMemory(name=shared_buffer_name)
 
     midi_in = mido.open_input('Frangitron virtual MIDI port', virtual=True)
 
     while True:
         try:
-            message = midi_in.receive(block=False)
+            message = midi_in.receive()
             if message is not None:
                 if message.type == 'control_change':
                     channel = (message.control - 1) + message.channel * 127
-                    shared_memory_buffer.buf[channel] = message.value * 2
+                    shared_memory.buf[channel] = message.value * 2
+                    print(list(shared_memory.buf[:20]))
 
         except KeyboardInterrupt:
             break
 
+    shared_memory.close()
     midi_in.close()
     print("Exiting")
