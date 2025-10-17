cffdaf594de2f7f39082a61001ace0e410220bf6
MrFrangipane <contact@frangipane.me>
2025-10-17 12:37:00 +0200

    dans le train

--- ledboardtranslatoremulator/central_widget.py
+++ ledboardtranslatoremulator/central_widget.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import QThread
-from PySide6.QtWidgets import QWidget, QGridLayout, QLabel
+from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QCheckBox
 
 from ledboardtranslatoremulator.emulator.renderer_widget import LedRendererEmulatorWidget
 from ledboardtranslatoremulator.io.io import IO, create_io_thread
@@ -19,14 +19,19 @@ class CentralWidget(QWidget):
 
         # FIXME use components singleton
         self.led_renderer_emulator = LedRendererEmulatorWidget(broadcaster=self.io.broadcaster)
-        layout.addWidget(self.led_renderer_emulator, 0, 0)
+        layout.addWidget(self.led_renderer_emulator, 1, 0)
         layout.setColumnStretch(0, 100)
-        layout.setRowStretch(0, 100)
+        layout.setRowStretch(1, 100)
 
         self.details_label = QLabel()
         self.led_renderer_emulator.detailsUpdated.connect(self.details_label.setText)
-        layout.addWidget(self.details_label, 0, 1)
+        layout.addWidget(self.details_label, 1, 1)
 
-        layout.addWidget(UpdateWidget(), 1, 0, 1, 2)
+        self.show_details_checkbox = QCheckBox("Show details")
+        self.show_details_checkbox.setChecked(True)
+        self.show_details_checkbox.stateChanged.connect(lambda state: self.details_label.setVisible(state == 2))
+        layout.addWidget(self.show_details_checkbox, 0, 0, 1, 2)
+
+        layout.addWidget(UpdateWidget(), 2, 0, 1, 2)
 
         self.io_thread.start()
